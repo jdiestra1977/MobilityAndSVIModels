8a7909b36f2d727b13c8f0ae6743f1040cbf4c0c
jdiestra1977 <diestra@gmail.com>
2022-08-23 09:24:03 -0500

    Nueva figura

--- mobilitySVIModels.R
+++ mobilitySVIModels.R
@@ -1,5 +1,9 @@
 library(tidyverse)
 
+#Personal access token: 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 
+#system("git remote set-url origin https://github.com/jdiestra1977/MobilityAndSVIModels.git")
+#system("git branch -M main")
+#system("git push -u origin main")
 #Correlation and best fit for Mobility ratio
 load("statsMobilityRatioAndSVI.RData")
 statsMobilityRatioAndSVI %>% head()
@@ -7,3 +11,8 @@ statsMobilityRatioAndSVI %>% head()
 statsMobilityRatioAndSVI %>% 
   ggplot(aes(x=as.Date(Days),y=Average))+
   geom_line() + facet_wrap(~Measure)
+
+load("totalVisitsByZipVsTime.RData")
+totalVisitsByZipVsTime %>%
+  ggplot(aes(x=as.Date(Date),y=TotalVisits,group=Zip))+
+  geom_line(size=1,alpha=07,color="gray")
