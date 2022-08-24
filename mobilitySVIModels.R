0ffb020893a51e1b47e8248c6b53eddd42fc0c53
jdiestra1977 <diestra@gmail.com>
2022-08-24 00:49:51 -0500

    08/24/2022

--- mobilitySVIModels.R
+++ mobilitySVIModels.R
@@ -4,15 +4,24 @@ library(tidyverse)
 #system("git remote set-url origin https://github.com/jdiestra1977/MobilityAndSVIModels.git")
 #system("git branch -M main")
 #system("git push -u origin main")
+
+load("totalVisitsByZipVsTime.RData")
+totalVisitsByZipVsTime %>%
+  ggplot(aes(x=as.Date(Date),y=InterVisitsOut+InterVisitsIn,group=Zip))+
+  theme_bw()+
+  geom_line(size=1,alpha=07,color="gray") +
+  ylab("Total visits between zip codes") + xlab("")+
+  theme(text=element_text(size=20))
+
 #Correlation and best fit for Mobility ratio
 load("statsMobilityRatioAndSVI.RData")
-statsMobilityRatioAndSVI %>% head()
-
 statsMobilityRatioAndSVI %>% 
   ggplot(aes(x=as.Date(Days),y=Average))+
   geom_line() + facet_wrap(~Measure)
 
-load("totalVisitsByZipVsTime.RData")
-totalVisitsByZipVsTime %>%
-  ggplot(aes(x=as.Date(Date),y=TotalVisits,group=Zip))+
-  geom_line(size=1,alpha=07,color="gray")
+load("statsDistanceRatioAndSVI.RData")
+statsDistanceRatioAndSVI %>%
+  ggplot(aes(x=as.Date(Days),y=Average,group=Measure))+
+  geom_line()+
+  facet_wrap(~Measure,scales = "free")
+
